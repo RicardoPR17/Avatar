6cd94e0d4fa332a46ec786a6f57e85ed73e886ef
JessicaDMunozO <jessitae@gmail.com>
2023-10-23 16:18:50 -0500

    Aplicación Na'vi inicial

---
+++ client/avatar-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+    return (
+        <main>
+            <div style={{ backgroundImage: "url(./logo.png)" }}>
+            </div>
+        </main>
+    )
+}
+
+export default App
