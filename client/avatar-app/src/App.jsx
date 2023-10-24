c5f6bfab5ab3b48d63fe51657daaae13968947e7
JessicaDMunozO <jessitae@gmail.com>
2023-10-23 19:24:36 -0500

    Componente Login

--- client/avatar-app/src/App.jsx
+++ client/avatar-app/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './App.css'
+import Login from './components/Login'
+
 
 const App = () => {
     return (
         <main>
-            <div style={{ backgroundImage: "url(./logo.png)" }}>
-            </div>
+            <Login />
         </main>
     )
 }
