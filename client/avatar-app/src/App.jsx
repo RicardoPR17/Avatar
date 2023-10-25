4fb8fa1fccf78827b1388a46c83509f5b04c937e
JessicaDMunozO <jessitae@gmail.com>
2023-10-24 20:21:39 -0500

    Componente Menu

--- client/avatar-app/src/App.jsx
+++ client/avatar-app/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './App.css'
-import Login from './components/Login'
-
+import Login from './components/Login/Login'
+import Menu from './components/Menu/Menu'
 
 const App = () => {
     return (
         <main>
             <Login />
+            <Menu />
         </main>
     )
 }
