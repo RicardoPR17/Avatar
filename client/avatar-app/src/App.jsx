dcd2cc5172f9090d44fe745291ba70efce0afd30
JessicaDMunozO <jessitae@gmail.com>
2023-10-24 21:55:46 -0500

    Route para navegar en la aplicación

--- client/avatar-app/src/App.jsx
+++ client/avatar-app/src/App.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
+
 import './App.css'
+
 import Login from './components/Login/Login'
 import Menu from './components/Menu/Menu'
+import Profile from './components/Profile/Profile'
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route } from 'react-router-dom'
 
 const App = () => {
     return (
         <main>
-            <Login />
-            <Menu />
+            <Router>
+                <Switch>
+                    <Route exact path="/">
+                        <Login />
+                    </Route>
+                    <Route path="/mainPage">
+                        <Menu />
+                    </Route>
+                    <Route path="/profile">
+                        <Menu />
+                        <Profile />
+                    </Route>
+                </Switch>
+            </Router>
         </main>
     )
 }
