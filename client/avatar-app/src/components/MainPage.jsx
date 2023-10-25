b4a078ef63eaee93d7129be87dc260fdf55dca5f
JessicaDMunozO <jessitae@gmail.com>
2023-10-24 20:22:53 -0500

    Base componente página principal

---
+++ client/avatar-app/src/components/MainPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MainPage = () => {
+    return(
+        <div className='principalPage'>
+            <h1>Top 10 cryptos today</h1>
+        </div>
+    )
+}
+
+export default MainPage
