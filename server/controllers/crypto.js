7ca44427310a06f493adf14d16e01f0b9e1d6b7d
RicardoPR17 <rpulidor17.rpr18@gmail.com>
2023-10-29 15:48:25 -0500

    Cambio error al no encontrar una criptomoneda

--- server/controllers/crypto.js
+++ server/controllers/crypto.js
@@ -54,10 +54,10 @@ const getOneCrypto = async (req, res) => {
       .project({ _id: 0, date: 1, "cryptocurrencies.$": 1 })
       .sort({ date: -1 })
       .toArray();
-    if (query.length === 0) throw "Crypto now found";
+    if (query.length === 0) throw "Cryptocurrency not found";
     res.json(query);
   } catch (err) {
-    res.status(404).json({ message: "Cryptocurrency not found" });
+    res.status(404).json({ error: err });
   }
 };
 
